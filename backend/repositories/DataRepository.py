aea584e1fc278d8fa8de83ec70379be862773559
CedricCallant <cedric.callant@student.howest.be>
2023-05-25 14:38:43 +0200

    Feature: LoginPage front

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -11,24 +11,7 @@ class DataRepository:
         return gegevens
 
     @staticmethod
-    def read_status_lampen():
-        sql = "SELECT * from lampen"
-        return Database.get_rows(sql)
-
-    @staticmethod
-    def read_status_lamp_by_id(id):
-        sql = "SELECT * from lampen WHERE id = %s"
-        params = [id]
-        return Database.get_one_row(sql, params)
-
-    @staticmethod
-    def update_status_lamp(id, status):
-        sql = "UPDATE lampen SET status = %s WHERE id = %s"
-        params = [status, id]
-        return Database.execute_sql(sql, params)
-
-    @staticmethod
-    def update_status_alle_lampen(status):
-        sql = "UPDATE lampen SET status = %s"
-        params = [status]
-        return Database.execute_sql(sql, params)
+    def read_cube_with_name(username):
+        sql = 'SELECT * FROM Cube WHERE username = %s'
+        params = [username]
+        Database.get_one_row(sql, params)
