2052d26cb473d1a70c14ea08df0cb00caa688021
CedricCallant <cedric.callant@student.howest.be>
2023-05-25 12:43:14 +0200

    frontend homepage afgewerkt

--- front/script/app.js
+++ front/script/app.js
@@ -1,11 +1,16 @@
 const lanIP = `${window.location.hostname}:5000`;
 const socketio = io(lanIP);
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
 const listenToUI = function () {
-  document.querySelector('.js-dropdown').addEventListener('click', function(){
-    options = document.querySelector('.js-showColorDD')
-    
-  })
 };
 
 const listenToSocket = function () {
